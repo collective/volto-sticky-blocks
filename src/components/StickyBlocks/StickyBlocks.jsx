9fe1e4c5ebef052a121225e408076c6bcadc0866
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-03-28 15:21:24 +0100

    feat: added container prop to StickyBlocks component and fix view

--- src/components/StickyBlocks/StickyBlocks.jsx
+++ src/components/StickyBlocks/StickyBlocks.jsx
@@ -3,12 +3,23 @@ import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { RenderBlocks } from '@plone/volto/components';
 
-const StickyBlocks = () => {
+const DefaultContainer = ({ children, className }) => (
+  <div className={className}>{children}</div>
+);
+
+const StickyBlocks = ({ container = DefaultContainer }) => {
   const location = useLocation();
   const blocks = useSelector(
-    (state) => state.content?.['@components']?.['sticky-blocks'],
+    (state) => state.content?.data?.['@components']?.['sticky-blocks'],
   );
 
-  return blocks ? <RenderBlocks content={blocks} location={location} /> : <></>;
+  const ContainerComponent = container;
+  return blocks ? (
+    <ContainerComponent className="sticky-blocks">
+      <RenderBlocks content={blocks} location={location} />
+    </ContainerComponent>
+  ) : (
+    <></>
+  );
 };
 export default StickyBlocks;
